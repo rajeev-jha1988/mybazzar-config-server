aedbfc9751615f3b49f2009521a7bb5167cca4bf
rajeev.jha <raj.k9732@gmail.com>
2018-03-17 18:15:38 +0530

    added config server
    Change-Id: Id9a8b43916ca999354265287171bfca880e74f19
    Signed-off-by: rajeev.jha <raj.k9732@gmail.com>

--- src/main/java/com/raj/mybazzar/config/ConfigServer.java
+++ src/main/java/com/raj/mybazzar/config/ConfigServer.java
@@ -2,11 +2,13 @@ package com.raj.mybazzar.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Hello world!
  *
  */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServer 
 {
