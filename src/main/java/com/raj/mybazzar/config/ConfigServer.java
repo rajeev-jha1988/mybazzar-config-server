88428188ea94d97c2fa8e6e7b403bc91d5194d93
rajeev.jha <raj.k9732@gmail.com>
2018-03-17 16:49:26 +0530

    my config server
    Change-Id: I8686779355c003325af404610b88d2747f49b93f
    Signed-off-by: rajeev.jha <raj.k9732@gmail.com>

---
+++ src/main/java/com/raj/mybazzar/config/ConfigServer.java
@@ -0,0 +1,17 @@
+package com.raj.mybazzar.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ConfigServer 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ConfigServer.class, args);
+    }
+}
